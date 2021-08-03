2fed898fd52fdbe46d04bcec29f9b6908b8197fc
RyanVanDijck <rvandijck02@outlook.com>
2021-08-03 16:16:45 +0100

    Passing second test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+
+const { models, sequelize } = require('./models/model.js');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }))
@@ -8,6 +11,13 @@ app.use(express.urlencoded({ extended: true }))
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
+app.post('/start', async (req, res) => {
+    const bookmarks = await models.Bookmark.findAll({})
+    console.log(bookmarks[0].url)
+    res.render('main.ejs', {
+        url: bookmarks[0].url
+    })
+})
   
 app.get('/', (req, res) => {
     res.render('index.ejs')

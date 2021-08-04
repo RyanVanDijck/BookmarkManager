c8c2d4bfcfd4bb3ba4b0fe162b820e05488147ac
RyanVanDijck <rvandijck02@outlook.com>
2021-08-04 16:13:00 +0100

    Adding option to delete individual bookmarks

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express');
 const bookmark = require('./models/bookmark.js');
+var methodOverride = require('method-override')
 const app = express()
 const port = 3000
 
 const { models, sequelize } = require('./models/model.js');
 
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }))
 
@@ -33,5 +35,11 @@ app.get('/start', async (req, res) => {
 app.post('/refresh', async (req, res) => {
     await models.Bookmark.destroy({ truncate : true, cascade: true })
 
+    res.redirect('/start')
+})
+
+app.delete('/start/:id', async (req, res) => {
+    console.log("Params:" + req.params.id)
+    await models.Bookmark.destroy({where:{id : req.params.id}})
     res.redirect('/start')
 })
